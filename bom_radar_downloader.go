e32866ff08ed615a4976fc7ddaf0ac082b951739
Amrullah Rahman <madramamrullah@gmail.com>
2024-09-29 20:35:00 +1000

    Initial commit

---
+++ bom_radar_downloader.go
@@ -0,0 +1,7 @@
+package bom_radar_downloader
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("First time doing this :3, lets see if it works")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hyperlinex/bom_radar_downloader
+
+go 1.21.12
