d081d4a64e771133edee3572a3b5561cff210508
Amrullah Rahman <madramamrullah@gmail.com>
2024-09-29 23:14:38 +1000

    testing

--- bom_radar_downloader.go
+++ bom_radar_downloader.go
@@ -1,7 +1,18 @@
 package bom_radar_downloader
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+// Eg. IDR66A == QLD Mt Stapylton 6m rain radar, see http://www.bom.gov.au/catalogue/anon-ftp.shtml for full list
 
 func Hello() {
 	fmt.Println("First time doing this :3, lets see if it works")
+}
+
+// Encode
+func Encode(t time.Time) string {
+	var str = "time is, " + t
+	return str
 }
